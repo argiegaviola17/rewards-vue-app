8fa612f4982e3d55272b9d68aaeb159d9fbb0b43
argie <argie.gaviola@synerteqsys.com>
2021-09-19 03:22:58 +0800

    added router link to main page in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,9 +21,10 @@
     </div>
 
     <div class="header_extra_small" >
-      
         <div class="logo_extra_small">
-            <img alt="APP logo" src="../assets/app-logo.png" style="width:100%;height:auto ">
+            <router-link :to="{ name: 'Home' }" style="text-decoration:none;color:black">
+                <img alt="APP logo" src="../assets/app-logo.png" style="width:100%;height:auto ">
+            </router-link>  
         </div>
         
         <div class="user_extra_small">
@@ -31,7 +32,7 @@
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
             </svg>
-            <label >Argie Gaviola</label>         
+            <label >Argie Gaviola</label>    
         </div>
     </div>
 </template>
